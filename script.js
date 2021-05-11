28901aaced5dca8fe1f29a45f3af5c9054f394fa
JawalArcilla <>
2021-05-11 19:59:46 +0000

    Fixing errors

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-document.getElementById('button').addEventListener('click',calculateArea)
+document.getElementById('button').addEventListener('click', calculateArea)
 let radius = 0
-let circumference = 0
-let diameter = 0
-
 
 function calculateArea () {
   radius = document.getElementById('radius').value
-  document.getElementById('answerA').innerHTML = 'The area is ' + (Math.PI * radius * radius)
+  document.getElementById('answer-a').innerHTML = 'The area is ' + (Math.PI * radius * radius)
 
   circumference = document.getElementById('radius').value
-  document.getElementById('answerC').innerHTML = 'The circumference is ' + (2 * radius * Math.PI)
+  document.getElementById('answer-c').innerHTML = 'The circumference is ' + (2 * radius * Math.PI)
 
   diameter = document.getElementById('radius').value
-  document.getElementById('answerD').innerHTML = 'The diameter is ' + (radius * 2)
+  document.getElementById('answer-d').innerHTML = 'The diameter is ' + (radius * 2)
 }
